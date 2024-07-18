ebfb10c328ed3b7e3552c0b8116f00a992ec35ee
MoryDog <s3881103@rmit.edu.vn>
2024-07-19 01:33:48 +0700

    setup spring boot project, organize folder structure

---
+++ backend/src/main/java/rmit/hoversprite/HoverSpriteApplication.java
@@ -0,0 +1,13 @@
+package rmit.team4.hover_sprite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoverSpriteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoverSpriteApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/rmit/hoversprite/HoverSpriteApplicationTests.java
@@ -0,0 +1,13 @@
+package rmit.team4.hover_sprite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoverSpriteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
