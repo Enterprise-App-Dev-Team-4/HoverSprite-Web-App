085a4fbf956ec03a649f9694d7db2e115238ff08
Dinh Minh <94245285+dinhminh0307@users.noreply.github.com>
2024-07-28 22:37:25 +0700

    bug: can not route to static web file spring yet

---
+++ backend/src/main/java/rmit/hoversprite/Config/WebConfig.java
@@ -0,0 +1,15 @@
+package rmit.hoversprite.Config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("file:../../../../../../frontend/src/Signup/");
+    }
+}
--- backend/src/main/java/rmit/hoversprite/Controller/LoginController.java
+++ backend/src/main/java/rmit/hoversprite/Controller/LoginController.java
@@ -1,16 +1,22 @@
 package rmit.hoversprite.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class LoginController {
-    @RequestMapping ("/login")
-    @ResponseBody
+    // @RequestMapping ("/login")
+    // @ResponseBody
 
-    public String returnHello()
-    {
-        return "<h2>Hello WOrld</h2>";
+    // public String returnHello()
+    // {
+    //     return "<h2>Hello WOrld</h2>";
+    // }
+
+    @GetMapping("/login")
+    public String getSignupPage() {
+        return "redirect:/Signup.html";
     }
 }
