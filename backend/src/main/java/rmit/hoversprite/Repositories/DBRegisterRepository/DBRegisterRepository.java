2ce0178ea5d28da6ccb7c073435db1a317528def
Dinh Minh <94245285+dinhminh0307@users.noreply.github.com>
2024-07-28 11:03:39 +0700

    feat:upload repository to backend

---
+++ backend/src/main/java/rmit/hoversprite/Repositories/DBRegisterRepository/DBRegisterRepository.java
@@ -0,0 +1,5 @@
+package rmit.hoversprite.Repositories.DBRegisterRepository;
+
+public class DBRegisterRepository {
+    
+}
---
+++ backend/src/main/java/rmit/hoversprite/Repositories/DBServiceRepository/DBServiceRepository.java
@@ -0,0 +1,5 @@
+package rmit.hoversprite.Repositories.DBServiceRepository;
+
+public class DBServiceRepository {
+    
+}
