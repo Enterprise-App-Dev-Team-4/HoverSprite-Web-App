a407252a04743206faa2db20cd6438892a3cc2bf
Evan Dinh <dinhgiabao430@gmail.com>
2024-08-09 22:41:48 +0700

    Update getter/setter

--- backend/src/main/java/rmit/hoversprite/Model/SprayerServices/SprayServices.java
+++ backend/src/main/java/rmit/hoversprite/Model/SprayerServices/SprayServices.java
@@ -17,4 +17,35 @@ public class SprayServices {
     private double price;
 
     // getter/ setter
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNameOfService() {
+        return nameOfService;
+    }
+
+    public void setNameOfService(String nameOfService) {
+        this.nameOfService = nameOfService;
+    }
+
+    public String getServiceType() {
+        return serviceType;
+    }
+
+    public void setServiceType(String serviceType) {
+        this.serviceType = serviceType;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
