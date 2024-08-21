1bfc5b96eebe2f502252523a5a5f12752903c4a6
Dinh Minh <94245285+dinhminh0307@users.noreply.github.com>
2024-08-22 02:13:34 +0700

    fix: resole bug form not send right data to server update

--- frontend/src/js/Profile.js
+++ frontend/src/js/Profile.js
@@ -42,34 +42,7 @@ function populateFormFields(data) {
     document.getElementById('phoneNumber').value = data.phoneNumber || '';
 }
 
-function uploadImage() {
-    // Firebase configuration for your web app
-    const firebaseConfig = {
-        apiKey: "AIzaSyD3hUpNFgAwXxlpsGs2sfI6Fgp3MOgXanA",
-        authDomain: "hoversprite-3d6b3.firebaseapp.com",
-        projectId: "hoversprite-3d6b3",
-        storageBucket: "hoversprite-3d6b3.appspot.com",
-        messagingSenderId: "375398691957",
-        appId: "1:375398691957:web:ce8d343181d0e843cef43c",
-        measurementId: "G-M1Q0WXPBE6"
-    };
-    
-    // Initialize Firebase
-    firebase.initializeApp(firebaseConfig);
-    const storage = firebase.storage();
 
-    const file = document.querySelector("#profileImageUpload").files[0];
-    if (!file) return Promise.resolve(null); // If no file is selected, resolve to null
-
-    const ref = storage.ref();
-    const name = +new Date() + "-" + file.name;
-    const metadata = { contentType: file.type };
-    const task = ref.child(name).put(file, metadata);
-
-    return task
-        .then(snapshot => snapshot.ref.getDownloadURL())
-        .catch(console.error);
-}
 
 function initializeProfileButtons() {
     const profileForm = document.querySelector('.profile-form');
@@ -104,11 +77,11 @@ function initializeProfileButtons() {
     profileForm.addEventListener('submit', function(e) {
         e.preventDefault();
 
-        uploadImage().then(profileImageUrl => {
+        uploadUserImage().then(profileImageUrl => {
             if (profileImageUrl) {
                 submitProfileFormWithImage(profileImageUrl);
             } else {
-              submitProfileFormWithImage(userData);
+              submitProfileFormWithImage(userData.profileImage);
             }
         });
     });
@@ -123,6 +96,7 @@ function submitProfileFormWithImage(imageUrl) {
         phoneNumber: document.getElementById('phoneNumber').value
     };
 
+    console.log(userProfile)
     sendRequestWithToken(UpdateProfileUrl, 'PUT', userProfile)
         .then(data => {
             console.log(data);
