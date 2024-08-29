4ce9cc19766654e2577793ff23f99de9bf2aa25b
Dinh Minh <94245285+dinhminh0307@users.noreply.github.com>
2024-08-29 16:38:52 +0700

    feat: update mail sender but not yet test :D

--- backend/src/main/java/rmit/hoversprite/Proxies/OrderConfirmEmailProxy.java
+++ backend/src/main/java/rmit/hoversprite/Proxies/OrderConfirmEmailProxy.java
@@ -1,5 +1,22 @@
 package rmit.hoversprite.Proxies;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
 public class OrderConfirmEmailProxy {
-    
+    @Autowired
+    private JavaMailSender mailSender;
+
+    public void sendSimpleEmail(String to, String subject, String text) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(text);
+        message.setFrom("admin@hoversprite.com");
+        mailSender.send(message);
+        System.out.println("email sent ");
+    }
 }
