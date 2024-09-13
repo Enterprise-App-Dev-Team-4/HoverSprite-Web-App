3f3ed5220c8fbf47d3a6c3a99387b9a0a06e3688
Dinh Minh <94245285+dinhminh0307@users.noreply.github.com>
2024-09-13 11:06:00 +0700

    add queerry code

---
+++ database/querry_code.sql
@@ -0,0 +1,12 @@
+select * from farmer_detail.sprayers;
+DELETE FROM farmer_detail.order_feedback WHERE feedbackid = 'FB001';
+DELETE FROM farmer_detail.feedback_sprayer WHERE feedbackid = 'FS001';
+ALTER TABLE feedback_sprayer_table
+DROP CONSTRAINT fkq3hew5oxtlhgvnldg2svi56g5;
+
+ALTER TABLE feedback_sprayer_table
+ALTER COLUMN feedbackid DROP NOT NULL;
+
+UPDATE feedback_sprayer_table
+SET feedbackid = NULL
+WHERE sprayer_id = 'SS001';
